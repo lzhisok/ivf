445b956aba02c286851952331fb911a1b0107baa
lzhisok <1589300156@qq.com>
2017-08-31 11:17:31 +0800

    修改代码

--- js/animation.js
+++ js/animation.js
@@ -1,36 +1,46 @@
 $(function() {
-	var width = 282;//获取单个子元素的宽度
+	var width = 282;//获取单个子元素的宽度;
 	var num = $('.adapt_crowd .box_list').children('li').length;//获取子元素数量
+	var showNum = num - 4;
 	var allWidth = width * num + 24 * num;
 	$('.adapt_crowd .box_list').width(allWidth);
 	var i = 0;
 
+	
+
 	$('#right').click(function() {
 		i++;
-		if(i<num) {
+		if(i<showNum) {
 			$('.adapt_crowd .box_list').css("left", -(width+24)*i);
+			$('#left').css('background-color', '#1ebcd9');
 		}
 		else{
-			i=num-1;
+			i=showNum;
 			$('.adapt_crowd .box_list').css("left", -(width+24)*i);
+			$('#right').css('background-color', '#c1c1c1');
 		}
 	});
 
 	$('#left').click(function() {
 		i--;
-		if(i>=0) {
+		if(i>0) {
 			$('.adapt_crowd .box_list').css("left", -(width+24)*i);
+			$('#right').css('background-color', '#1ebcd9');
 		}
 		else{
 			i=0;
 			$('.adapt_crowd .box_list').css("left", 0);
+			$('#left').css('background-color', '#c1c1c1');
 		}
 	});
+	
+
 
 	// doctors图片滚动
 	var doctorWidth = 264,
 		doctorUl = $(".doctors_list_container .doctors_list ul");
 		doctorNum = $(".doctors_list_container .doctors_list ul").children(".doctor_box").length,
+		doctorShowNum = doctorNum - 4;
 		doctorsAllWidth = (doctorWidth + 8) * doctorNum,
 		j=0;
 
@@ -38,24 +48,27 @@ $(function() {
 
 	$("#right_doctor").click(function() {
 		j++;
-		if(j<doctorNum) {
-			// $(".doctors_list_container .doctors_list ul").css('left', -(doctorWidth + 8)*j);
-			doctorUl.animate({left: -(doctorWidth + 8)*j}, "slow");
+		if(j<doctorShowNum) {
+			doctorUl.animate({left: -(doctorWidth + 8)*j}, 500);
+			$('#left_doctor').css('background-color', '#1ebcd9');
 		}
 		else{
-			j=doctorNum-1;
-			doctorUl.animate({left: -(doctorWidth + 8)*j}, "slow");
+			j=doctorShowNum;
+			doctorUl.animate({left: -(doctorWidth + 8)*j}, 500);
+			$('#right_doctor').css('background-color', '#c1c1c1');
 		}
 	});
 
 	$('#left_doctor').click(function() {
 		j--;
-		if(j>=0) {
-			doctorUl.animate({left: -(doctorWidth + 8)*j}, "slow");
+		if(j>0) {
+			doctorUl.animate({left: -(doctorWidth + 8)*j}, 500);
+			$('#right_doctor').css('background-color', '#1ebcd9');
 		}
 		else{
 			j=0;
-			doctorUl.animate({left: 0}, "slow");
+			doctorUl.animate({left: 0}, 500);
+			$('#left_doctor').css('background-color', '#c1c1c1');
 		}
 	})
 })
